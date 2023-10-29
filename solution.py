b729273b4bcd010c5cfe1b9b047ea75150e24e7d
nubol23 <rafael.rvp98@gmail.com>
2023-10-29 00:06:45 -0400

    Setup lab 2

---
+++ solution.py
@@ -0,0 +1,14 @@
+def precios(edad: int):
+    # Cambiar el valor de esta variable como corresponde
+    precio = None
+    
+    return precio
+
+
+def triangulo(n: int):
+   res = ""
+
+   # Concatenar cada linea a la variable `res`
+
+   return res
+
